e04d42e12c2833f0906bc58b0f449f582eacc89e
apokharel123 <Student@Bluegrasss-MacBook-Pro-2.local>
2022-03-02 23:44:31 -0500

    Initial Exercise

---
+++ script.js
@@ -0,0 +1,45 @@
+//Math Literal Exercise
+
+//1a
+let warmHUgs="Hi, I\'m Olaf and I like warm hugs."
+//1b
+console.log(warmHUgs.toUpperCase());
+//1c
+console.log(warmHUgs.replace("like","love"));
+
+//2A
+beenImpaled="Oh, look at that. I\'ve been impaled."
+//  2B
+console.log(beenImpaled.slice(18));  
+
+//3A
+let dotDotDot="..."
+//3B
+skullBones = `I don\'t have a skull ${dotDotDot} or bones`;
+console.log(skullBones);
+
+//4
+console.log(Math.PI);
+
+//5
+let randomNumber=Math.random();
+ randomNumber*=3;
+ randomNumber=Math.ceil(randomNumber)
+ console.log(randomNumber);
+
+ //Bonus
+ //6
+ console.log(" Let It go!".repeat(3). toUpperCase());
+ 
+ //7A
+let reindeers="Reindeers are better than people."
+//7B
+console.log(reindeers.replace("Reindeers are better than people.","Reindeers_are_better_than_people."));
+
+//8
+n= Math.sqrt(2)
+console.log(n);
+
+//9
+let newRandomNumber= Math.ceil((Math.random()*15)+8);
+console.log(newRandomNumber);
